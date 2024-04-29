c1fa15b7c8d2dbfc7c61af4fcede3c63d0817591
unknown <revan.arturito@gmail.com>
2024-04-29 07:44:56 +0700

    Menambahkan API Mahasiswa dan penjelasan singkat

---
+++ modul10_1302223057/Controllers/MahasiswaController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace modul10_1302223057.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MahasiswaController
+    {
+        // inisialisasi data mahasiswa berupa anggota kelompok
+        private static List<Mahasiswa> mahasiswa = new List<Mahasiswa>()
+        {
+            new Mahasiswa("Cokorda Arturito Revan Putra Diarta", "1302223057", new List<String> {"PBO", "KPL", "BASDAT"} , 2022),
+            new Mahasiswa("Rd. M. Faisal Ramadhan Junaidi", "1302220093", new List<String> {"PBO", "KPL", "BASDAT"}, 2022),
+            new Mahasiswa("Farrel Haykal Giffari", "1302220064", new List<String> {"PBO", "KPL", "BASDAT"}, 2022),
+        };
+
+        // menggunakan IEnumerable untuk menampilkan seluruh data list Mahasiswa
+        [HttpGet(Name = "GetMahasiswa")]
+        public IEnumerable<Mahasiswa> Get() 
+        { 
+            return mahasiswa; 
+        }
+
+        // menggunakan post untuk menambah data mahasiwa pada list
+        [HttpPost]
+        public void post([FromBody] Mahasiswa ListMahasiswa) 
+        {
+            mahasiswa.Add(ListMahasiswa);
+        }
+
+        // menampilkan data mahasiswa berdasarkan id(index) yang diinput
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int  id)
+        {
+            return mahasiswa[id];
+        }
+
+        // menggunakan delete untuk menghapus data list berdasarkan id
+        [HttpDelete]
+        public void delete(int id)
+        {
+            mahasiswa.RemoveAt(id);
+        }
+
+    }
+}
---
+++ modul10_1302223057/Mahasiswa.cs
@@ -0,0 +1,24 @@
+﻿namespace modul10_1302223057
+{
+    // deklarasi class mahasiswa
+    public class Mahasiswa
+    {
+        // menambahakan atribut nama
+        public String Name {  get; set; }
+        // menambahkan atribut nim
+        public String Nim {  get; set; }
+        // menambahkan atribut berupa list matkul yang diambil
+        public List<String> Course { get; set; }
+        // menambahkan atribut tahun
+        public int Year { get; set; }
+
+        // constructor class Mahasiswa
+        public Mahasiswa(string name, string nim, List<string> course, int year)
+        {
+            this.Name = name;
+            this.Nim = nim;
+            this.Course = course;
+            this.Year = year;
+        }
+    }
+}
